6c62a9ece5848c65d0d2864fcc5aadae2b3c59b7
AmanMahor21 <amanmahor021@gmail.com>
2024-05-29 02:13:50 +0530

    hello put

--- src/App.js
+++ src/App.js
@@ -29,6 +29,7 @@ function App() {
         </div>
         <div className="d-flex justify-content-between">
           <LeftSideRadio />
+          hello
           {stepNumber === 1 && <InterviewForm />}
           {stepNumber === 2 && <JobDetails />}
           {stepNumber === 3 && <PersonalDetails />}
